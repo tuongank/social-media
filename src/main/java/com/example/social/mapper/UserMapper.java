63bbb363a64b1dc738309b7204ab2d8d31050ddb
Tuong Anh <98013554+Tuoank@users.noreply.github.com>
2024-12-05 15:17:09 +0700

    mapper dto vs entity

--- src/main/java/com/example/social/mapper/UserMapper.java
+++ src/main/java/com/example/social/mapper/UserMapper.java
@@ -1,4 +1,21 @@
 package com.example.social.mapper;
 
+import com.example.social.dto.UserDto;
+import com.example.social.entity.User;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .imageUrl(user.getImageUrl())
+                .address(user.getAddress())
+                .bio(user.getBio())
+                .role(user.getRole().name())
+                .build();
+    }
 }
